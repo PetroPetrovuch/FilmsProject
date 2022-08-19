989d9c4c874fc42ee727bf87d82a022739a1afab
PetroPetrovuch <Petrovu412@gmail.com>
2022-08-19 21:06:59 +0300

    Застосування умов,циклiв

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,37 @@
-console.log("HEllo FilmsProject");
+"use strict";
+
+let numberOFFilms = +prompt("Скільки фільмів ви переглянули?",0);
+
+
+const personalMovieDB = {
+
+    count : numberOFFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+
+
+
+
+for(let i=0;i<personalMovieDB.count;i++){
+    const film = prompt("Останій переглянутий фільм?","");
+    const rating = prompt("На скільки ви його оцінюєте","");
+
+    if(film!=null && rating!=null && film !="" && rating!="" && film.length < 50)
+    {
+        personalMovieDB.movies[film]=rating;
+        console.log("done");
+    }
+    else
+    {
+        console.log("error");
+        i--;
+    }
+    
+
+}
+
+console.log(personalMovieDB);
